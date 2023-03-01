0ebbe33a3f038719893fd69d71901570fed2334a
Shafali Gupta <shafaligupta31@gmail.com>
2023-02-28 21:56:30 -0500

    added code from class

---
+++ src/components/Attractions.js
@@ -0,0 +1,11 @@
+//import './Attractions.css';
+
+function Attractions() {
+  return (
+    <div className="content">
+      This page will have information on Glove World, Jellyfishing, and Goo Lagoon.
+    </div>
+  );
+}
+
+export default Attractions;
---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+//import './Homepage.css';
+
+function Homepage() {
+  return (
+    <div className="content">
+      This is the landing page for the site. 
+      <h1 style={{fontFamily: "SpongeboyRegular", color: "blue"}}>Welcome to Bikini Bottom!</h1>
+    </div>
+  );
+}
+
+export default Homepage;
