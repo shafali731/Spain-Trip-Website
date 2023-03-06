15eadacc87d75586cb64a2f0434a2b2f42ea0804
Shafali Gupta <shafaligupta31@gmail.com>
2023-03-05 20:30:35 -0500

    added styling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,7 +8,7 @@ function Homepage() {
       <div className='headerBack'>
         <h1 style={{ fontFamily: "Courgette", color: "rgb(170, 21, 27)", fontSize: "70px", justifyContent: "center", display: "flex", padding: "2px" }}>Welcome to Spain!</h1>
       </div>
-      <div className='row'>
+      <div className='row' >
         <div class="leftside">
 
           <p className='writing'> Welcome to your journey of Spain. The first part of booking a trip is figuring
@@ -21,13 +21,12 @@ function Homepage() {
         </div>
         {/* Photo by <a href="https://unsplash.com/@jorgefdezsalas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jorge Fernández Salas</a> on <a href="https://unsplash.com/s/photos/spain?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
    */}
-        <div className='homeImage'>
+        {/* <div className='homeImage'>
           <img src={homeImage} alt="Spain" />
-        </div>
-        <div class="rightside">
+        </div> */}
+        {/* <div class="rightside">
 
           <p className='writing'> 
-          {/* To navigate our website choose any of the links in the navigation bar on the top.  */}
           <ul>
             <li>Hotels will take you to the different hotels we picked out for you</li>
             <li>
@@ -36,16 +35,11 @@ function Homepage() {
           can see different places to visit.</li>
           <li>Whenever you're lost you can always go home and return back 
           to this page</li>
-          {/* <li></li> */}
           </ul>
-          {/* Hotels will take you to the different hotels we picked out for you. Restaurants
-          will show you the top restaurants in our opinoin. And finally, attractions is where you
-          can see different places to visit. Whenever you're lost you can always go home and return back 
-          to this page <br></br><br></br> */}
   
           </p>
 
-        </div>
+        </div> */}
       </div>
     </div>
   );
